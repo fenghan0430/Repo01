2cb85fe5d50a127e486b0b5af73472e1c477e056
fenghan0430 <2730230535@qq.com>
2022-10-10 19:38:51 +0800

    更改了字符的错误，删除了不必要文字

--- VD3.java
+++ VD3.java
@@ -11,7 +11,6 @@ public class VD3 {
         //long 很大很大
         //long 类型的变量最后要加L
         long d = 10;
-        System.out.println(d); 
 
 
         //浮点
@@ -23,9 +22,12 @@ public class VD3 {
         double f = 1.1;
 
         //字符
-        //char
-        char g = '你好';
+        //char 单个字符
+        char g = 'a';
         
+        //string 字符串，多字符
+        String g2 = "世界";
+
         //布尔
         //boolean
         boolean h = true;
