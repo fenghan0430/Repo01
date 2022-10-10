b4cb01de91398590ca19faf28ef0ebc6fc39bc85
fenghan0430 <2730230535@qq.com>
2022-10-10 14:55:31 +0800

    添加了浮点，字符，布尔

--- VD3.java
+++ VD3.java
@@ -1,5 +1,7 @@
 public class VD3 {
     public static void main(String[] args) {
+
+        //整数
         //byte -128~127
         byte a = 10;
         //short -32768~32767
@@ -10,5 +12,22 @@ public class VD3 {
         //long 类型的变量最后要加L
         long d = 10;
         System.out.println(d); 
+
+
+        //浮点
+        //float
+        //定义float类型变量要加F为后缀
+        float e = 10.1F;
+
+        //double
+        double f = 1.1;
+
+        //字符
+        //char
+        char g = '你好';
+        
+        //布尔
+        //boolean
+        boolean h = true;
     }   
 }
