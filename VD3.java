1992389ebe937d2fec5fb93b83e3d0f29f623a15
fenghan0430 <2730230535@qq.com>
2022-10-10 14:25:24 +0800

    数字类型训练

---
+++ VD3.java
@@ -0,0 +1,12 @@
+public class VD3 {
+    public static void main(String[] args) {
+        //byte -128~127
+        byte a = 10;
+        //short -32768~32767
+        short b = 10;
+        //int -21亿~21亿
+        int c = 10;
+        //long 很大很大
+        long d = 10;    
+    }   
+}
